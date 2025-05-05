1c5521ba211fbc762cd7067f8ae99be6310a1bc8
Sviatoslav <djsv91@gmail.com>
2025-05-05 15:41:32 +0300

    Create task and tag forms

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,13 +3,15 @@ from tracker.views import (
     TaskListView,
     TagListView,
     TaskCreateView,
+    TagCreateView,
 )
 
 urlpatterns = [
     #    path("admin/", admin.site.urls),
     path("", TaskListView.as_view(), name="task-list"),
-    path("taskcreate/", TaskCreateView.as_view(), name="task-create"),
+    path("task_create/", TaskCreateView.as_view(), name="task-create"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tag_create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "tracker"
